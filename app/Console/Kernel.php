4cbf9480cc165ca6f8bce72cc392d98b577c8b36
Jesús Cova <jesuscova@jisparking.com>
2023-11-06 07:01:47 -0300

    Configurando .env

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('app:send-notifications')->hourly();
+        $schedule->command('app:send-notifications')->everyMinute();
 
         // $schedule->command('inspire')->hourly();
     }
