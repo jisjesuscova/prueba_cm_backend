1d2a15969ec922acf3694b86a546958c07aead9d
Jesús Cova <jesuscova@jisparking.com>
2023-11-07 06:47:16 -0300

    cambios

--- app/Console/Commands/SendNotifications.php
+++ app/Console/Commands/SendNotifications.php
@@ -6,6 +6,8 @@ use Illuminate\Console\Command;
 
 use App\Models\Notification;
 
+use App\Models\Token;
+
 class SendNotifications extends Command
 {
     /**
@@ -37,50 +39,52 @@ class SendNotifications extends Command
 
         $SERVER_API_KEY = env('FIREBASE_API_KEY');
 
-        $token = env('FIREBASE_APP_TOKEN');
+        $tokens = Token::select('token')->get();
+        
+        foreach ($tokens as $token) {
+            $data = [
     
-        $data = [
+                "registration_ids" => [
+                    $token->token
+                ],
+        
+                "notification" => [
+        
+                    "title" => 'Title Number '.$notification_quantity,
     
-            "registration_ids" => [
-                $token
-            ],
+                    "body" => 'Message Number '.$notification_quantity,
+        
+                    "sound"=> "default" // required for sound on ios
+        
+                ],
+        
+            ];
+        
+            $dataString = json_encode($data);
+        
+            $headers = [
+        
+                'Authorization: key=' . $SERVER_API_KEY,
+        
+                'Content-Type: application/json',
+        
+            ];
+        
+            $ch = curl_init();
+
+            curl_setopt($ch, CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send');
     
-            "notification" => [
-    
-                "title" => 'Title Number '.$notification_quantity,
-    
-                "body" => 'Message Number '.$notification_quantity,
-    
-                "sound"=> "default" // required for sound on ios
-    
-            ],
-    
-        ];
-    
-        $dataString = json_encode($data);
-    
-        $headers = [
-    
-            'Authorization: key=' . $SERVER_API_KEY,
-    
-            'Content-Type: application/json',
-    
-        ];
-    
-        $ch = curl_init();
-    
-        curl_setopt($ch, CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send');
-    
-        curl_setopt($ch, CURLOPT_POST, true);
-    
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-    
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $dataString);
-    
-        $response = curl_exec($ch);
+            curl_setopt($ch, CURLOPT_POST, true);
+        
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $dataString);
+        
+            $response = curl_exec($ch);
+        }
     }
 }
