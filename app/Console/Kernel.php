8935853e894132e8ba754eb586f1a7dd92306641
Jesús Cova <jesuscova@jisparking.com>
2023-11-06 23:39:47 -0300

    cambios

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('app:send-notifications')->everyMinute();
+        $schedule->command('app:send-notifications')->everyTenMinutes();
 
         // $schedule->command('inspire')->hourly();
     }
