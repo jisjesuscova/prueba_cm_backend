b8fdb1a8db322421cea0fec924398ec59d289682
Jesús Cova <jesuscova@jisparking.com>
2023-11-07 01:25:02 -0300

    cambios

--- app/Http/Controllers/Api/NotificationController.php
+++ app/Http/Controllers/Api/NotificationController.php
@@ -98,25 +98,29 @@ class NotificationController extends Controller
             ];
         
             $ch = curl_init();
-        
-            curl_setopt($ch, CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send');
-        
-            curl_setopt($ch, CURLOPT_POST, true);
-        
-            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $dataString);
-        
-            $response = curl_exec($ch);
 
-            return response()->json([
-                'success' => true,
-                'data' => $notification
-            ], 200);
+            $tokens = Token::select('token')->get();
+            
+            foreach ($tokens as $token) {
+                curl_setopt($ch, CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send');
+        
+                curl_setopt($ch, CURLOPT_POST, true);
+            
+                curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            
+                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $dataString);
+            
+                $response = curl_exec($ch);
+
+                return response()->json([
+                    'success' => true,
+                    'data' => $notification
+                ], 200);
+            }
         } catch (\Exception $e) {
             return response()->json([
                 'success' => false,
